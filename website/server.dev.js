5817ccc9a6eeb8ce659f0ddb94c70e91ff6ae716
Sander <114917575+Drillbeary@users.noreply.github.com>
2024-02-06 10:04:04 +0100

    Added error handling for server-side
    removed server.dev from git ignore

--- website/server.dev.js
+++ website/server.dev.js
@@ -1,4 +1,5 @@
 // server.js
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
@@ -7,20 +8,15 @@ const port = process.env.PORT || 8080;
 const YT_Channel_ID = "UCuNwM9dSfTwCdlNDJswjm2Q" // Jarle channel id
 
 
-
 // fetch youtube videos
 
 app.get('/fetch-youtube-videos', async (req, res) => {
     try {
         // fetch max 3 videos, order by date, type video as snippet
-        const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${YT_Channel_ID}&maxResults=3&order=date&type=video&key=${process.env.YOUTUBE_API_KEY}`);
+        const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${YT_Channel_ID}&maxResults=3&order=date&type=video&key=${process.env.YOUTUBE_API_KEY_DEV}`);
       const videos = response.data.items;
       res.json(videos);
     } catch (error) {
-      if (error.response && error.response.data) {
-        // If the error response has a data property, include it in the error object
-        error.message += ' - ' + JSON.stringify(error.response.data);
-      }
       console.error(error);
       res.status(500).json({ error: error.toString() });
     }
@@ -30,14 +26,17 @@ app.get('/fetch-youtube-videos', async (req, res) => {
 
 // facebook so complicated :(
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.listen(port, () => {
-console.log(`Server is running on port ${port}`);
-});
+    console.log(`Server is running on port ${port}`);
+    });
+
+
+// Serve static files from the React app
+//app.use(express.static(path.join(__dirname, 'build')));
+
+//app.get('/', (req, res) => {
+//res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//});
+
+
+
